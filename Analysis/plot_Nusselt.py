2cc26acf6158210fab0a43a714cf7dfa7a2bf50f
Dudin Danil <danil.dudin@vis-technology.ru>
2024-11-18 21:59:43 +0500

    Рефакторинг для графика с Нуссольтом

--- Analysis/plot_Nusselt.py
+++ Analysis/plot_Nusselt.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
-import numpy as np
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import pandas as pd
 import os
 
@@ -10,20 +10,16 @@ def plot_surf(path_name):
     ax = fig.add_subplot(projection='3d')
 
     df = pd.read_csv("./datasets/" + path_name)
-    E = df.e.values
-    V = df.v.values
-    e = np.unique(E)
-    v = np.unique(V)
-    Nu = df.Nu.values
-    Nu = Nu.reshape(len(e), len(v))
-    V, E = np.meshgrid(v, e)
-    surf = ax.plot_surface(V, E, Nu, rstride=1, cstride=1, cmap=cm.coolwarm,
-                        linewidth=0, antialiased=False)
+    surf = ax.plot_trisurf(df.v, df.e, df.Nu, cmap=cm.inferno, linewidth=0.5, antialiased=False)
 
     ax.set_xlabel("v")
     ax.set_ylabel("e")
     ax.set_zlabel("Nu")
+
     fig.colorbar(surf, shrink=0.5, aspect=10)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    fig.tight_layout()
 
     name_dir = f'image/Nusselt'
     if not os.path.exists(name_dir):
