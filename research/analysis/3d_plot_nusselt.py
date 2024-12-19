4f78afd9665b9d08270e519738948b05a9219496
Danil Dudin <danil.dudin@vis-technology.ru>
2024-12-20 00:33:01 +0500

    правка 3d графика

--- research/analysis/3d_plot_nusselt.py
+++ research/analysis/3d_plot_nusselt.py
@@ -2,15 +2,14 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
 import pandas as pd
-import os
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 
-def plot_surf(path_name):
+def plot_surf(name_dir, name_file):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
-    df = pd.read_csv("./datasets/" + path_name)
-    print(df)
+    df = pd.read_csv(f"./datasets/{name_dir}/{name_file}")
     E = df.e.values
     V = df.v.values
     e = np.unique(E)
@@ -18,15 +17,19 @@ def plot_surf(path_name):
     Nu = df.Nu.values
     Nu = Nu.reshape(len(e), len(v))
     V, E = np.meshgrid(v, e)
-    surf = ax.plot_surface(V, E, Nu, rstride=1, cstride=1, cmap=cm.coolwarm,
-                        linewidth=0, antialiased=False)
+    surf = ax.plot_trisurf(df.v, df.e, df.Nu, cmap=cm.inferno, linewidth=0.5, antialiased=False)
 
     ax.set_xlabel("e")
     ax.set_ylabel("v")
     ax.set_zlabel("Nu")
+
     fig.colorbar(surf, shrink=0.5, aspect=10)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    fig.tight_layout()
+
     plt.show()
 
 
 import sys
-plot_surf(sys.argv[1])
+plot_surf(sys.argv[1], sys.argv[2])
