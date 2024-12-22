ec592c3492a22a0bffdd2e13fc024bb00eee04a6
Danil Dudin <danil.dudin@vis-technology.ru>
2024-12-22 08:25:25 +0500

    Увеличил точность и подправил логику записи в файл

--- odeslib/include/eq_sys.h
+++ odeslib/include/eq_sys.h
@@ -1,38 +1,38 @@
 #pragma once
 
-#define _USE_MATH_DEFINES
+
 #include <math.h>
 
-const double Pr = 100.0;
-const double r = 0.0;
-const double k = 0.962;
-const double b = 4.0 / (1.0 + k * k);
-const double d = (4.0 + k * k) / (1.0 + k * k);
+const long double PI = 3.14159265358979323851;
+const long double Pr = 100.0;
+const long double k = 0.962;
+const long double b = 4.0 / (1.0 + k * k);
+const long double d = (4.0 + k * k) / (1.0 + k * k);
 
-double X(double e, double v, double t, const double y[]) {
-	double c = cos(2.0 * M_PI * v * t);
-	return Pr * (-y[0] + r * y[1] + e * y[4] * c * c);
+long double X(long double e, long double v, long double t, const long double y[]) {
+	long double c = cos(2.0 * PI * v * t);
+	return Pr * (-y[0] + e * y[4] * c * c);
 }
 
-double Y(double e, double v, double t, const double y[]) {
+long double Y(long double e, long double v, long double t, const long double y[]) {
 	return -y[1] + y[0] + y[0] * y[2];
 }
 
-double Z(double e, double v, double t, const double y[]) {
+long double Z(long double e, long double v, long double t, const long double y[]) {
 	return -b * y[2] - y[0] * y[1];
 }
 
-double V(double e, double v, double t, const double y[]) {
+long double V(long double e, long double v, long double t, const long double y[]) {
 	double c = cos(2.0 * M_PI * v * t);
-	return Pr * (-d * y[3] + (r * y[4] - e * y[1] * c * c) / d);
+	return - Pr * (d * y[3] + e * y[1] * c * c / d);
 }
 
-double W(double e, double v, double t, const double y[]) {
+long double W(long double e, long double v, long double t, const long double y[]) {
 	return -d * y[4] + y[3];
 }
 
-double Nu(double e, double v, double t, const double y[]) {
+long double Nu(long double e, long double v, long double t, const long double y[]) {
 	return y[2];
 }
 
-double (*sys[6])(double, double, double, const double*) = {X, Y, Z, V, W, Nu};
+long double (*sys[6])(long double, long double, long double, const long double*) = {X, Y, Z, V, W, Nu};
--- odeslib/src/odes_lib.cpp
+++ odeslib/src/odes_lib.cpp
@@ -3,40 +3,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-const double A[6] = { 0.0, 1.0 / 4.0, 3.0 / 8.0, 12.0 / 13.0, 1.0, 1.0 / 2.0 };
-const double B[6][6] = {
+const long double A[6] = { 0.0, 1.0 / 4.0, 3.0 / 8.0, 12.0 / 13.0, 1.0, 1.0 / 2.0 };
+const long double B[6][6] = {
     { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 },
     { 1.0 / 4.0, 0.0, 0.0, 0.0, 0.0, 0.0 },
     { 3.0 / 32.0, 9.0 / 32.0, 0.0, 0.0, 0.0, 0.0 },
     { 1932.0 / 2197.0, -7200.0 / 2197.0, 7296.0 / 2197.0, 0.0, 0.0, 0.0 },
     { 439.0 / 216.0, -8.0, 3680.0 / 513.0, -845.0 / 4104.0, 0.0, 0.0 },
     { -8.0 / 27.0, 2.0, -3544.0 / 2565.0, 1859.0 / 4104.0, -11.0 / 40.0, 0.0 } };
-const double C[6] = { 16.0 / 135.0, 0.0, 6656.0 / 12825.0, 28561.0 / 56430.0, -9.0 / 50.0, 2.0 / 55.0 };
-const double C_HAT[6] = { 25.0 / 216.0, 0.0, 1408.0 / 2565.0, 2197.0 / 4104.0, -1.0 / 5.0, 0.0 };
+const long double C[6] = { 16.0 / 135.0, 0.0, 6656.0 / 12825.0, 28561.0 / 56430.0, -9.0 / 50.0, 2.0 / 55.0 };
+const long double C_HAT[6] = { 25.0 / 216.0, 0.0, 1408.0 / 2565.0, 2197.0 / 4104.0, -1.0 / 5.0, 0.0 };
 
 
-void Add(double arr1[], double s, int size) {
+void Add(long double arr1[], long double s, int size) {
     for (int i = 0; i < size; i++) {
         arr1[i] += s;
     }
 }
 
-void Init(double arr1[], double s, int size) {
+void Init(long double arr1[], long double s, int size) {
     for (int i = 0; i < size; i++) {
         arr1[i] = s;
     }
 }
 
-void Copy(double arr1[], const double arr2[], int size) {
+void Copy(long double arr1[], const long double arr2[], int size) {
     for (int i = 0; i < size; i++) {
         arr1[i] = arr2[i];
     }
 }
 
-double Norm(const double arr1[], const double arr2[], int size) {
-    double norm = 0.0;
+long double Norm(const long double arr1[], const long double arr2[], int size) {
+    long double norm = 0.0;
     for (int i = 0; i < size; i++) {
-        double r = arr1[i] - arr2[i];
+        long double r = arr1[i] - arr2[i];
         norm += r * r;
     }
     return sqrt(norm);
@@ -54,37 +54,37 @@ extern "C" {
 
 MODULE_API int solve_rkf45(char* path_file, double consts_init[8], double t0,
     double t_end, double tol = 1e-6, double h_init = 0.1,
-    double h_min = 1e-6, double h_max = 1.0, double output_step = 0.01) {
+    double h_min = 1e-6, double h_max = 1.0, double output_step = 0.0) {
 
     FILE* fp = fopen(path_file, "a+");
     if (!fp) {
         return -1;
     }
 
-    double t = t0;
-    const double e = consts_init[0];
-    const double v = consts_init[1];
-    double h = h_init;
-    double next_output_time = t0; 
+    long double t = t0;
+    const long double e = consts_init[0];
+    const long double v = consts_init[1];
+    long double h = h_init;
+    long double next_output_time = t0; 
 
     const int num_funs = 6;
-    double y[num_funs];
+    long double y[num_funs];
     for (int i = 0; i < num_funs; i++) {
         y[i] = consts_init[i + 2];
     }
 
-    double k[6][num_funs];
+    long double k[6][num_funs];
     while (t < t_end) {
         if (t + h > t_end) {
             h = t_end - t;
         }
 
         for (int i = 0; i < 6; i++) {
-            double ti = t + A[i] * h;
+            long double ti = t + A[i] * h;
             for (int p = 0; p < num_funs; p++) {
-                double yi[num_funs];
+                long double yi[num_funs];
                 Copy(yi, y, num_funs);
-                double sum = 0.0;
+                long double sum = 0.0;
                 if (i > 0) {
                     for (int j = 0; j < i; j++) {
                         sum += B[i][j] * k[j][p];
@@ -96,11 +96,11 @@ MODULE_API int solve_rkf45(char* path_file, double consts_init[8], double t0,
             }
         }
 
-        double y4[num_funs];
-        double y5[num_funs];
+        long double y4[num_funs];
+        long double y5[num_funs];
 
-        double sum1[num_funs];
-        double sum2[num_funs];
+        long double sum1[num_funs];
+        long double sum2[num_funs];
         Init(sum1, 0.0, num_funs);
         Init(sum2, 0.0, num_funs);
         for (int i = 0; i < num_funs; i++) {
@@ -112,20 +112,26 @@ MODULE_API int solve_rkf45(char* path_file, double consts_init[8], double t0,
             y5[i] = y[i] + h * sum2[i];
         }
 
-        double error = Norm(y5, y4, num_funs);
+        long double error = Norm(y5, y4, num_funs);
 
         if (error < tol) {
-            t += h;
             Copy(y, y5, num_funs);
 
-            if (t >= next_output_time) {
-                fprintf(fp, "%lf, ", t);
+            if (abs(t - next_output_time) <= 10e-16 || output_step == 0.0) {
+                fprintf(fp, "%Lf, ", t);
                 for (int i = 0; i < num_funs - 1; i++) {
-                    fprintf(fp, "%lf, ", y[i]);
+                    fprintf(fp, "%Lf, ", y[i]);
                 }
-                fprintf(fp, "%lf\n", y[num_funs - 1]);
+                fprintf(fp, "%Lf\n", y[num_funs - 1]);
                 next_output_time += output_step;
+                t += h;
+            } else if (t > next_output_time) {
+                t = t - t + next_output_time;
+                
+            } else {
+                t += h;
             }
+            
         }
 
         if (error > 0.0) {
@@ -137,7 +143,7 @@ MODULE_API int solve_rkf45(char* path_file, double consts_init[8], double t0,
 	return 0;
 }
 
-MODULE_API int nusselt_number(char* path_file, double E[3], double V[3], double init[6], double t0,
+MODULE_API int nusselt_number(char* path_file, long double E[3], long double V[3], long double init[6], double t0,
     double t_end, double tol = 1e-6, double h_init = 0.1,
     double h_min = 1e-6, double h_max = 1.0) {
 
@@ -146,26 +152,27 @@ MODULE_API int nusselt_number(char* path_file, double E[3], double V[3], double 
         return -1;
     }
 
-    double h = h_init;
+    long double h = h_init;
 
     const int num_funs = 6;
-    double k[6][num_funs];
-    for (double e = E[0]; e <= E[1]; e += E[2]) {
-        for (double v = V[0]; v <= V[1]; v += V[2]) {
+    long double k[6][num_funs];
+    
+    for (long double e = E[0]; e <= E[1]; e += E[2]) {
+        for (long double v = V[0]; v <= V[1]; v += V[2]) {
 
-            double y[num_funs];
+            long double y[num_funs];
             Copy(y, init, num_funs);
-            for (double t = t0; t < t_end;) {
+            for (long double t = t0; t < t_end;) {
                 if (t + h > t_end) {
                     h = t_end - t;
                 }
 
                 for (int i = 0; i < 6; i++) {
-                    double ti = t + A[i] * h;
+                    long double ti = t + A[i] * h;
                     for (int p = 0; p < num_funs; p++) {
-                        double yi[num_funs];
+                        long double yi[num_funs];
                         Copy(yi, y, num_funs);
-                        double sum = 0.0;
+                        long double sum = 0.0;
                         if (i > 0) {
                             for (int j = 0; j < i; j++) {
                                 sum += B[i][j] * k[j][p];
@@ -177,11 +184,11 @@ MODULE_API int nusselt_number(char* path_file, double E[3], double V[3], double 
                     }
                 }
 
-                double y4[num_funs];
-                double y5[num_funs];
+                long double y4[num_funs];
+                long double y5[num_funs];
 
-                double sum1[num_funs];
-                double sum2[num_funs];
+                long double sum1[num_funs];
+                long double sum2[num_funs];
                 Init(sum1, 0.0, num_funs);
                 Init(sum2, 0.0, num_funs);
                 for (int i = 0; i < num_funs; i++) {
@@ -193,7 +200,7 @@ MODULE_API int nusselt_number(char* path_file, double E[3], double V[3], double 
                     y5[i] = y[i] + h * sum2[i];
                 }
 
-                double error = Norm(y5, y4, num_funs);
+                long double error = Norm(y5, y4, num_funs);
 
                 if (error < tol) {
                     t += h;
@@ -206,10 +213,10 @@ MODULE_API int nusselt_number(char* path_file, double E[3], double V[3], double 
                 h = fmax(fmin(h, h_max), h_min);
             }
 
-            fprintf(fp, "%lf, ", e);
-            fprintf(fp, "%lf, ", v);
-            double nu = 1.0 - 2.0 * y[num_funs - 1] / (t_end - t0);
-            fprintf(fp, "%lf\n", nu);
+            fprintf(fp, "%Lf, ", e);
+            fprintf(fp, "%Lf, ", v);
+            long double nu = 1.0 - 2.0 * y[num_funs - 1] / (t_end - t0);
+            fprintf(fp, "%Lf\n", nu);
         }
     }
     
