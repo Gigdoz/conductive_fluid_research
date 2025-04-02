d888ff33b4dcc11dcc06812cc0009ef269db0b00
Dudin Danil <danil.dudin@vis-technology.ru>
2025-04-03 02:13:24 +0500

    Добавлены скрипты для карты динамических режимов

---
+++ build_map_dinamic_modes/map_dinamic_modes.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+name_file = "data/puancare/puancare_map e=50-150; v=3-5.csv"
+df = pd.read_csv(name_file)
+
+columns = df.columns
+matrix = df.pivot(index=columns[0], columns=columns[1], values=columns[2])
+e_values = matrix.index.astype(float)
+v_values = matrix.columns.astype(float)
+map_data = matrix.values
+
+# --- Визуализация карты ---
+plt.figure(figsize=(8, 6))
+plt.imshow(map_data, origin='lower', extent=(v_values.min(), v_values.max(), e_values.min(), e_values.max()), aspect='auto', cmap='plasma')
+
+label = "Количество уникальных точек Пуанкаре"
+plt.colorbar(label=label)
+plt.title("Карта динамических режимов")
+plt.xlabel("v")
+plt.ylabel("e")
+plt.show()
---
+++ build_map_dinamic_modes/solve_puancare.py
@@ -0,0 +1,80 @@
+import numpy as np
+from math import *
+import pandas as pd
+import csv
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+from tqdm import tqdm
+
+# build_map_dinamic_modes/puancare.py
+
+# --- Параметры системы ---
+T_max = 80     # Время интегрирования
+T_transient = 8   # Время для исключения переходного процесса
+dt = 0.01        # Шаг дискретизации при регистрации точек Пуанкаре
+
+# --- Диапазоны параметров для карты ---
+P1_range = np.linspace(50.0, 150.0, 100)  # Диапазон значений P1
+P2_range = np.linspace(5.0, 7.5, 250)  # Диапазон значений P2
+
+# --- Плоскость Пуанкаре (пересечение по x1 = 0) ---
+section_value = 0.0
+section_index = 0  # Индекс переменной, по которой проверяется пересечение
+
+# --- Уравнения системы (пример) ---
+
+Pr = 100.0
+k = 0.962
+b = 4/(1+k**2)
+d = (4+k**2)/(1+k**2)
+
+def system(t, state, e, v):
+    X, Y, Z, V, W = state
+
+    cos_2 = (cos(2.0*pi*v*t))**2
+    dX = Pr*(-X+e*W*cos_2)
+    dY = -Y+X+X*Z
+    dZ = -b*Z-X*Y
+    dV = -Pr*(d*V+e*Y*cos_2/d)
+    dW = -d*W+V
+    return [dX, dY, dZ, dV, dW]
+
+# --- Функция для численного интегрирования и поиска точек Пуанкаре ---
+def poincare_map(P1, P2, state):    
+    # Интегрируем систему до T_transient, чтобы исключить переходный процесс
+    sol0 = solve_ivp(system, [0, T_transient], state, args=(P1, P2), t_eval=[T_transient])
+    state = sol0.y.T[0]
+    
+    # Интегрируем основную систему и собираем точки Пуанкаре
+    times = np.arange(0, T_max, dt)
+    sol = solve_ivp(system, [0, T_max], state, args=(P1, P2), t_eval=times)
+    
+    X = sol.y.T
+    poincare_points = []
+    
+    for i in range(1, len(X)):
+        # Поиск пересечения с плоскостью Пуанкаре x1 = section_value
+        if (X[i-1][section_index] < section_value and X[i][section_index] >= section_value):
+            poincare_points.append(X[i][1:3])  # Сохраняем только x2 и x3 для анализа
+
+    return np.array(poincare_points)
+
+# Сохранение начальных условий для продолжение вычесления
+X0 = np.random.rand(5) * 2 - 1
+
+file = open("data/puancare/puancare_map.csv", mode="w", newline="")
+writer = csv.writer(file)
+writer.writerow(["e", "v", "amounts"])
+
+for i, P1 in enumerate(tqdm(P1_range, desc="Building Map")):
+    for j, P2 in enumerate(P2_range):
+        points = []
+        
+        # Если точек мало, считаем режим регулярным
+        amount = 0 # Регулярный режим (синий)
+        if len(points) >= 20:
+            # Считаем количество уникальных точек (порог P2_max*0.01 для группировки близких точек)
+            unique_points = np.unique(np.round(points, decimals=2), axis=0)
+            amount = len(unique_points)
+        writer.writerow([P1, P2, amount])
+
