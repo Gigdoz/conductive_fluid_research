8de78f6dca48633d67469e47dfd602e86af9a6be
Dudin Danil <danil.dudin@vis-technology.ru>
2025-05-13 01:47:44 +0500

    добавил вейвлет спектр

--- pp.py
+++ pp.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fft import fft, fftfreq
-from scipy import signal
+import pywt
 
 from system import SolveOptions, SolveSYS, system
 
@@ -82,7 +82,13 @@ def fft_fr(data, time):
 
 def plot_fft(X, e, v):
     key, val = X.popitem()
-    options = SolveOptions("", "", 0, 300, 100, 0.001, False, atol=1e-6, rtol=1e-3)
+
+    t_0 = 0
+    T_tran = 100
+    t_end = 200
+    dt = 0.001
+
+    options = SolveOptions("", "", t_0, t_end, T_tran, dt, False, atol=1e-6, rtol=1e-3)
     solve_sys = SolveSYS(options, None)
     state = [2, 0, 0, 0, 0, 0]
     time, data = solve_sys.solve(state, e, v)
@@ -95,7 +101,7 @@ def plot_fft(X, e, v):
 
     # plot time signal:
     axs["signal"].set_title(f'e={e}; v={v}\nSignal')
-    axs["signal"].plot(time[:int(50/0.0001)], data[0, :int(50/0.0001)], lw=0.3, color='C0')
+    axs["signal"].plot(time[:int(100/dt)], data[0, :int(100/dt)], lw=0.3, color='C0')
     axs["signal"].set_xlabel("Time")
     axs["signal"].set_ylabel("X")
 
@@ -108,53 +114,27 @@ def plot_fft(X, e, v):
     axs["magnitude"].set_xlabel("frequency")
     axs["magnitude"].set_ylabel(f'{key}')
 
+    # plot phase
     axs["phase"].set_title("Phase")
     axs["phase"].plot(data[0], data[3], '.', ms=0.4, color='black')
     axs["phase"].set_xlabel("X")
     axs["phase"].set_ylabel('V')
 
-    # widths = np.arange(0.001, Xf.max())
-    # cwtmatr = signal.cwt(data[0], signal.ricker, widths)
-    # cwtmatr_yflip = np.flipud(cwtmatr)
+    # plot wavelet
+    # dfreq = 5/((t_end - t_0) - T_tran)
+    # frequencies = np.arange(dfreq, 20, dfreq)
+    # scales = pywt.central_frequency('morl')/(frequencies*dt)
+    # coef, freqs = pywt.cwt(data[0, :int(50/dt)], scales, 'morl', sampling_period=dt)
     # axs["wavelet"].set_title("Wavelet")
-    # axs["wavelet"].imshow(cwtmatr_yflip, extent=[time[0], time[-1], 0.001, Xf.max()],
-    #                       cmap='plasma', aspect='auto',
-    #                         vmax=abs(cwtmatr).max(),
-    #                         vmin=-abs(cwtmatr).min())
+    # img = axs["wavelet"].imshow(np.abs(coef), extent=[T_tran, T_tran+50, freqs[-1], freqs[0]],
+    #        cmap='viridis', aspect='auto')
     # axs["wavelet"].set_xlabel("Time")
     # axs["wavelet"].set_ylabel('frequency')
+    # cbar = fig.colorbar(img, ax=axs["wavelet"])
+    # cbar.set_label('Амплитуда')
 
-# counter_plot += phase_plotXT(0, 170, 5.65, 1)
-# counter_plot += phase_plotXT(0, 170, 5.65, 1+1/5.65)
-# counter_plot += phase_plotXT(0, 170, 5.65, 1+2/5.65)
-# counter_plot += phase_plotXT(0, 170, 5.65, 1+3/(2*5.65))
-
-# counter_plot += phase_plotXX(0, 160, 4.3)
-# counter_plot += phase_plotXX(1, 160, 4.3)
-# counter_plot += phase_plotXX(2, 160, 4.3)
-# counter_plot += phase_plotXX(3, 160, 4.3)
-# counter_plot += phase_plotXX(4, 160, 4.3)
-
-# counter_plot += phase_plot2d([0, 1], 160, 3.1)
-# counter_plot += phase_plot2d([0, 2], 160, 3.1)
-# counter_plot += phase_plot2d([0, 3], 160, 3.1)
-# counter_plot += phase_plot2d([0, 4], 160, 3.1)
-
-
-# counter_plot += phase_plot3d([0, 1, 4], 160, 6.3)
-# counter_plot += phase_plot3d([1, 2, 3], 160, 6.3)
-# counter_plot += phase_plot3d([1, 2, 4], 160, 6.3)
-# counter_plot += phase_plot3d([1, 3, 4], 160, 6.3)
-
-# седло 1 2 4
-
-# counter_plot += plot(0, 295, 4.17)
-# counter_plot += plot(0, 295, 4.19)
-# counter_plot += plot(0, 295, 4.22)
-# counter_plot += plot(0, 295, 4.24)
-
-
-plot_fft({"X" : 0}, 230, 5.65)
+plot_fft({"X" : 0}, 160, 3.1015)
 # 7.335
+# 160, 3.1015 - переход через перемежаемость 1 рода
 
 plt.show()
